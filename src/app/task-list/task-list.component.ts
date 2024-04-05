c495c1acd8c85337754c0eb1913d88e8bc54d884
Alma Orco <coma5@hotmail.com>
2024-04-06 00:58:31 +0200

    Vinculando el check a la propiedad completed

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,17 +1,24 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 import { TASKS } from '../task-mock';
 import { Task } from '../task';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, UpperCasePipe, NgIf],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-taskList:Task[] = TASKS;
+  taskList:Task[] = TASKS;
 
+  selectedTask:Task = {id:0, title: ""};
+
+  onChange(task: Task): void {
+    task.completed = !task.completed;
+    this.selectedTask = task;
+  }
 
 }
--- src/app/task-mock.ts
+++ src/app/task-mock.ts
@@ -1,14 +1,10 @@
 import { Task } from './task';
 
 export const TASKS: Task[] = [
-  { id: 1, title: 'Dr Nice' },
-  { id: 2, title: 'Narco' },
-  { id: 3, title: 'Bombasto' },
-  { id: 4, title: 'Celeritas' },
-  { id: 5, title: 'Magneta' },
-  { id: 6, title: 'RubberMan' },
-  { id: 7, title: 'Dynama' },
-  { id: 8, title: 'Dr IQ' },
-  { id: 9, title: 'Magma' },
-  { id: 10, title: 'Tornado' }
+  { id: 1, title: 'Vincular el check a la propiedad completed', completed: true },
+  { id: 2, title: 'Separar tareas completadas de no completadas' },
+  { id: 3, title: 'Cambiar formato a las no completadas' },
+  { id: 4, title: 'Encapsular los itemList en un componente hijo' },
+  { id: 5, title: 'Crear formulario para crear/modificar tareas' },
+  { id: 6, title: 'Buscar la manera de guardar la info' }
 ];

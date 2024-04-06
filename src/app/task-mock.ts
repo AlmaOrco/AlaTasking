3d97854b9a7b51168ed80509621d9eb11d5f3524
Alma Orco <coma5@hotmail.com>
2024-04-06 23:48:43 +0200

    Formato de las tareas tachadas

--- src/app/task-mock.ts
+++ src/app/task-mock.ts
@@ -3,7 +3,7 @@ import { Task } from './task';
 export const TASKS: Task[] = [
   { id: 1, title: 'Vincular el check a la propiedad completed', completed: true },
   { id: 2, title: 'Separar tareas completadas de no completadas', completed: true },
-  { id: 3, title: 'Tachar formato a las completadas' },
+  { id: 3, title: 'Tachar formato a las completadas', completed: true },
   { id: 4, title: 'Encapsular los itemList en un componente hijo' },
   { id: 5, title: 'Crear formulario para crear/modificar tareas' },
   { id: 6, title: 'Buscar la manera de guardar la info' }

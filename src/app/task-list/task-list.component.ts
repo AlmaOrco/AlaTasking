ec82bd53d9fed4b0af8c6a3c67844500c87fad47
Alma Orco <coma5@hotmail.com>
2024-04-06 13:53:30 +0200

    Crear separar tareas completadas y no completadas

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { TASKS } from '../task-mock';
 import { Task } from '../task';
 import { NgModel } from '@angular/forms';
@@ -11,14 +11,29 @@ import { NgModel } from '@angular/forms';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit{
   taskList:Task[] = TASKS;
+  toDoList:Task[] = [];
+  doneList:Task[] = [];
 
   selectedTask:Task = {id:0, title: ""};
+  
+  ngOnInit(): void {
+    this.orderList(TASKS);
+  }
 
   onChange(task: Task): void {
     task.completed = !task.completed;
     this.selectedTask = task;
+    this.orderList(this.taskList);
+  }
+
+  orderList(taskList:Task[]):void {
+    this.toDoList = [];
+    this.doneList = [];
+    taskList.forEach(task => {
+      task.completed ? this.doneList.push(task) : this.toDoList.push(task);
+    });
   }
 
 }
--- src/app/task-mock.ts
+++ src/app/task-mock.ts
@@ -2,8 +2,8 @@ import { Task } from './task';
 
 export const TASKS: Task[] = [
   { id: 1, title: 'Vincular el check a la propiedad completed', completed: true },
-  { id: 2, title: 'Separar tareas completadas de no completadas' },
-  { id: 3, title: 'Cambiar formato a las no completadas' },
+  { id: 2, title: 'Separar tareas completadas de no completadas', completed: true },
+  { id: 3, title: 'Tachar formato a las completadas' },
   { id: 4, title: 'Encapsular los itemList en un componente hijo' },
   { id: 5, title: 'Crear formulario para crear/modificar tareas' },
   { id: 6, title: 'Buscar la manera de guardar la info' }
